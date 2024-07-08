75e968949de6fd58b136337376f7cd699fb3f30e
SudhansuSekharJena <sudhansu.sekhar.jena.24@gmail.com>
2024-07-08 20:27:31 +0530

    move the backend code to backend folder

--- NotifyMe/utils/websocket_utils.py
+++
@@ -1,7 +0,0 @@
-from channels.layers import get_channel_layer
-from asgiref.sync import async_to_sync
-
-
-class NotificationManager:
-  pass
-
--- api/urls.py
+++
@@ -1,9 +0,0 @@
-from NotifyMe.views.views import UserAPI, SubscriptionAPI
-from django.contrib import admin
-from django.urls import path
-
-# url patterns are list of urls
-urlpatterns = [
-  path("user", UserAPI.as_view()),
-  path("subscription", SubscriptionAPI.as_view()),
-]
